09487238146633e01382feff2627ad13a6274797
Menno van Grinsven <scanno71@gmail.com>
2019-03-16 21:43:35 +0100

    nile-common: Add power hal

--- nile.mk
+++ nile.mk
@@ -310,7 +310,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.1-service-qti \
+		power.sdm660
 
 # QCOM
 PRODUCT_COPY_FILES += \
